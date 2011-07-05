6c576f6d2b27c749c14a9d451b989894d8422a01
Nat Duca <natduca@gmail.com>
2011-07-04 17:07:16 -0700

    First impl of db.

--- quickopen/db.py
+++ quickopen/db.py
@@ -11,8 +11,125 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import collections
+import os
+import re
 
 class DB(object):
   def __init__(self, settings):
     self.settings = settings
+    self.settings.register('dirs', list, [])
+    self._dir_cache = _DirCache()
+    self._dirty = True
+
+  @property
+  def dirs(self):
+    return list(self.settings.dirs)
+
+  def add_dir(self, dir):
+    cur = list(self.settings.dirs)
+    if dir in cur:
+      return
+    # commit change
+    cur.append(dir)
+    self.settings.dirs = cur
+    self._set_dirty()
+
+  def _set_dirty(self):
+    self._dirty = True
+
+  def sync(self):
+    """Ensures database index is up-to-date"""
+    if not self._dirty:
+      return
+    sync = _DBSync(self.settings.dirs, self._dir_cache)
+    sync.run()
+    self._files = sync.files
+    self._dirty = False
+
+  def search(self,query_regex):
+    rc = re.compile(query_regex)
+
+    if self._dirty:
+      self.sync()
+
+    res = []
+    truncated = False
+    for x in self._files:
+      if rc.search(x):
+        res.append(x)
+    return res
+
+class _DirEnt(object):
+  def __init__(self, st_mtime, ents):
+    self.st_mtime = st_mtime
+    self.ents = ents
+
+class _DirCache(object):
+  def __init__(self):
+    self.dirs = dict()
+    self.rel_to_real = dict()
+
+  def reset_realpath_cache(self):
+    self.rel_to_real = dict()
+
+  def realpath(self, d):
+    if d in self.rel_to_real:
+      return self.rel_to_real[d]
+    else:
+      r = os.path.realpath(d)
+      self.rel_to_real[d] = r
+      return r
+
+  def listdir(self, d):
+    """Lists contents of a dir, but only using its realpath."""
+    if d in self.dirs:
+      de = self.dirs[d]
+      if os.stat(d).st_mtime == de.st_mtime:
+        return de.ents
+      else:
+        logging.info("directory %s changed", d)
+        del self.dirs[d]
+    st_mtime = os.stat(d).st_mtime
+    ents = os.listdir(d)
+    de = _DirEnt(st_mtime, ents)
+    self.dirs[d] = de
+    return de.ents
+
+class _DBSync(object):
+  def __init__(self, dirs, dir_cache):
+    self.dir_cache = dir_cache
+    self.dirs = dirs
+    self.dir_cache.reset_realpath_cache()    
+    self.files = set()
+    self.pending = collections.deque()
+
+    self.visited = set()
+
+    # enqueue start points in reverse because the whole search is DFS
+    reverse_dirs = list(dirs)
+    reverse_dirs.reverse()
+    for d in reverse_dirs:
+      self.enqueue_dir(d)
+
+  def step(self):
+    d = self.pending.popleft()
+    ents = [self.dir_cache.realpath(os.path.join(d, ent)) for ent in self.dir_cache.listdir(d)]
+    for ent in ents:
+      if os.path.isdir(ent):
+        self.enqueue_dir(ent)
+      else:
+        print "found %s" % ent
+        self.files.add(ent)
     
+  def run(self):
+    while len(self.pending):
+      self.step()
+
+  def enqueue_dir(self, d):
+    dr = self.dir_cache.realpath(d)
+    if dr in self.visited:
+      return
+    self.visited.add(dr)
+    self.pending.appendleft(dr)
+
--- quickopen/db_test.py
+++ quickopen/db_test.py
@@ -12,16 +12,73 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import db
+import os
+import settings
 import tempfile
 import unittest
 
-
-class DBTest_EmptySettings(unittest.TestCase):
+class DBTest(unittest.TestCase):
   def setUp(self):
     self.settings_file = tempfile.NamedTemporaryFile()
-    self.db_ = db_stub.DB(settings.Settings(self.settings_file.name))
+    self.settings = settings.Settings(self.settings_file.name)
+    self.db = db.DB(self.settings)
+
+    # create a directory hierarchy to do tests in
+    self.test_data_dir = os.path.realpath(os.path.join(tempfile.gettempdir(), 'db_test'))
+    if os.path.exists(self.test_data_dir):
+      os.system('rm -rf %s' % self.test_data_dir)
+    os.system('cp -r ./test_data/ %s' % self.test_data_dir)
+
+    # dir symlink project1_symlink to project1
+    src = os.path.join(self.test_data_dir, 'project1')
+    dst = os.path.join(self.test_data_dir, 'project1_symlink')
+    os.system('ln -s %s %s' % (src, dst))
+
+    # file symlink something/foo.txt to project1/something/foo.txt
+    src = os.path.join(self.test_data_dir, 'project1/foo.txt')
+    dst = os.path.join(self.test_data_dir, 'something/foo.txt')
+    os.system('ln -s %s %s' % (src, dst))
+
 
   def tearDown(self):
+    if os.path.exists(self.test_data_dir):
+      os.system('rm -rf %s' % self.test_data_dir)
     self.settings_file.close()
 
-  def test_add_nested_dir():
+  def test_nonexistant_dir(self):
+    self.db.add_dir(self.test_data_dir)
+    bad_dir = os.path.join(self.test_data_dir, 'xxx')
+    self.db.sync()
+
+  def test_add_nested_dir_doesnt_dup(self):
+    self.db.add_dir(self.test_data_dir)
+    sub_dir = os.path.join(self.test_data_dir, 'project1')
+    self.db.add_dir(sub_dir)
+    hits = self.db.search('MySubSystem.c')
+    self.assertEquals(1, len(hits))
+    self.assertEquals(os.path.join(self.test_data_dir, 'project1/MySubSystem.c'), hits[0])
+
+  def test_search_unique(self):
+    self.db.add_dir(self.test_data_dir)
+    hits = self.db.search('MySubSystem.c')
+    self.assertEquals(1, len(hits))
+    self.assertEquals(os.path.join(self.test_data_dir, 'project1/MySubSystem.c'), hits[0])
+
+  def test_partial_search(self):
+    self.db.add_dir(self.test_data_dir)
+    hits = self.db.search('MyClass')
+    self.assertTrue(len(hits) >= 2)
+    self.assertTrue(os.path.join(self.test_data_dir, 'project1/MyClass.c') in hits)
+    self.assertTrue(os.path.join(self.test_data_dir, 'project1/MyClass.h') in hits)
+
+  def test_dir_symlinks_dont_dup(self):
+    self.db.add_dir(self.test_data_dir)
+
+  def test_search_more_than_max(self):
+    # find something where you get more than a sane number of requests
+    pass
+
+  def test_add_file_after_scan(self):
+    # not implemented
+    pass
+

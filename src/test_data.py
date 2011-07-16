d9ba3260b6283a9a3d521e2c948aa708e2d2e90a
Nat Duca <natduca@gmail.com>
2011-07-16 00:54:06 -0700

    Use subprocess instead of os.system so we dont spew messages to stdout

--- src/test_data.py
+++ src/test_data.py
@@ -13,38 +13,46 @@
 # limitations under the License.
 import os
 import tempfile
+import shlex
+import subprocess
 
 class TestData(object):
   def __init__(self):
     # create a directory hierarchy to do tests in
     self.test_data_dir = os.path.realpath(os.path.join(tempfile.gettempdir(), 'db_test'))
     if os.path.exists(self.test_data_dir):
-      os.system('rm -rf %s' % self.test_data_dir)
-    os.system('cp -r ./test_data/ %s' % self.test_data_dir)
+      self.system('rm -rf %s' % self.test_data_dir)
+    self.system('cp -r ./test_data/ %s' % self.test_data_dir)
 
     # dir symlink project1_symlink to project1
     src = os.path.join(self.test_data_dir, 'project1')
     dst = os.path.join(self.test_data_dir, 'project1_symlink')
-    ret = os.system('ln -s %s %s' % (src, dst))
+    ret = self.system('ln -s %s %s' % (src, dst))
     assert ret == 0
 
     # file symlink something/foo.txt to project1/something/foo.txt
     src = os.path.join(self.test_data_dir, 'project1/foo.txt')
     dst = os.path.join(self.test_data_dir, 'something/foo.txt')
-    ret = os.system('ln -s %s %s' % (src, dst))
+    ret = self.system('ln -s %s %s' % (src, dst))
     assert ret == 0
 
     # make a real git directory
     gitpath = os.path.join(self.test_data_dir, 'gitproj')
     oldcwd = os.getcwd()
     os.chdir(gitpath)
-    ret = os.system('git init')
-    ret = os.system('git add .')
-    ret = os.system('git commit -m .')
+    ret = self.system('git init')
+    ret = self.system('git add .')
+    ret = self.system('git commit -m .')
     assert ret == 0
     os.chdir(oldcwd)
 
+  def system(self, cmd):
+    args = shlex.split(cmd)
+    p = subprocess.Popen(args,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+    p.communicate()
+    return p.returncode
+    
   def close(self):
     if os.path.exists(self.test_data_dir):
-      os.system('rm -rf %s' % self.test_data_dir)
+      self.system('rm -rf %s' % self.test_data_dir)
 

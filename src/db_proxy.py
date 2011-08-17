2b851640d15ac549a75e6e11353caa49412c97ed
Nat Duca <nduca@chromium.org>
2011-08-16 17:06:06 -0700

    Remove db import.

--- src/db_proxy.py
+++ src/db_proxy.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import async_http_connection
-import db
 import httplib
 import subprocess
 import sys

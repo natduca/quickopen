451b50f93be56cbb67528955365c6f85bda203e5
Nat Duca <natduca@gmail.com>
2012-03-18 19:47:04 -0700

    Fix prelaunch test

--- src/prelaunch_test.py
+++ src/prelaunch_test.py
@@ -26,7 +26,7 @@ class PrelaunchTest(unittest.TestCase, QuickopenTestBase):
   def qo(self, cmd, *args):
     full_args = [cmd]
     full_args.extend(args)
-    return prelaunch.run_command_in_existing(self.daemon.host, self.daemon.port, full_args)
+    return prelaunch_client.run_command_in_existing(self.daemon.host, self.daemon.port, full_args)
 
   def turn_off_daemon(self):
     self.daemon.close()

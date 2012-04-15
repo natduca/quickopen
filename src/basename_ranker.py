81e8bb66066a9a88e7331810e0545f37b49bf86c
Nat Duca <natduca@gmail.com>
2012-04-15 13:32:58 -0700

    Remove dead code

--- src/basename_ranker.py
+++ src/basename_ranker.py
@@ -61,63 +61,6 @@ class BasenameRanker(object):
     s_lower = s.lower()
     return [s_lower[i] for i in starts]
 
-  def get_num_hits_on_word_starts_old(self, query, orig_hit, truncated = False):
-    starts = self.get_starts(orig_hit)
-    if len(starts) == 0:
-      return 0
-    hit = orig_hit.lower()
-    start_letters = [hit[i] for i in starts]
-
-    # escape the query
-    lower_query = query.lower()
-    # go xyz --> (.*)x(.*)y(.*)z(.*)
-    tmp = ['(.*?)']
-    for i in range(len(lower_query)):
-      tmp.append('(%s.*)' % re.escape(lower_query[i]))
-    flt = ''.join(tmp)
-    m = re.match(flt, hit)
-    if not m:
-      if len(query) > 1:
-        return  self.get_num_hits_on_word_starts(query[:-1],hit,True)
-      return 0
-    ngroups = len(tmp) - 1
-
-    groups = m.groups()
-    group_starts = [0]
-    for i in range(1,len(groups)):
-      group_starts.append(group_starts[i-1] + len(groups[i-1]))
-
-    score = 0
-
-    # you get one point for matching the first letter
-    try:
-      if len(groups[0]) == 0 and groups[1][0] == hit[starts[0]]:
-        score += 1
-    except:
-      raise Exception("error while processing q=%s h=%s" % (query, hit))
-
-    # you get one point for every matching letter that was a start letter
-    hits = 0
-    for i in range(1, len(groups)):
-      # look for start_letter to the left of group_start[i]
-      # that matches groups[i][0]
-      if len(groups[i]) == 0:
-        continue
-      letter_to_look_for = groups[i][0]
-      leftmost_start_index = group_starts[i]
-      for j in range(len(start_letters)):
-        if starts[j] < leftmost_start_index:
-          continue
-        if start_letters[j] == letter_to_look_for:
-          score += 1
-          hits += 1
-
-    # you get +1 if you match every letter in the word
-    if hits == len(start_letters) == ngroups and not truncated:
-      score += 1
-    return score
-
-
   def rank_query(self, query, candidate, truncated = False, debug = False):
     basic_rank, num_word_hits = self._get_basic_rank(query, candidate, debug)[:2]
 

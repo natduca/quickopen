8f3aa77c21feabe175381be0e957d6bc79eda0dd
Nat Duca <nduca@chromium.org>
2012-12-28 00:16:47 -0800

    Nearly all unitests pass using chrome-app backend

--- src/message_loop_chrome.py
+++ src/message_loop_chrome.py
@@ -26,6 +26,10 @@ import chromeapp
 
 _pending_tasks = []
 _quit_handlers = []
+_is_main_loop_running = False
+_unittests_running = False
+_active_test = None
+_active_test_result = None
 
 def supported():
   # OSX only for now, since GTK is solid elsewhere.
@@ -46,20 +50,48 @@ def post_task(cb, *args):
       "args": args})
 
 def post_delayed_task(cb, delay, *args):
-  raise NotImplementedException()
+  # TODO(nduca): Make this behave properly iff strictly needed it.
+  _pending_tasks.append({
+      "cb": cb,
+      "args": args})
 
 def is_main_loop_running():
-  return False
+  return _is_main_loop_running
 
 def init_main_loop():
-  raise NotImplementedException()
+  return
 
 def run_main_loop():
+  if _unittests_running and not _active_test:
+    _current_main_loop_instance += 1 # kill any enqueued tasks
+    del _quit_handlers[:]
+    raise Exception("UITestCase must be used for tests that use the message_loop.")
+
   global _pending_tasks
+  global _is_main_loop_running
+  _is_main_loop_running = True
   while len(_pending_tasks):
     t = _pending_tasks[0]
     _pending_tasks = _pending_tasks[1:]
-    t["cb"](*t["args"])
+    try:
+      t["cb"](*t["args"])
+    except:
+      exc, value, tb = sys.exc_info()
+      if _active_test:
+        import unittest
+        if isinstance(value, unittest.TestCase.failureException):
+          _active_test_result.addFailure(_active_test, (exc, value, tb))
+        else:
+          if not str(value).startswith("_noprint"):
+            print "Untrapped exception! Exiting message loop with exception."
+          _active_test_result.addError(_active_test, (exc, value, tb))
+        quit_main_loop()
+        return
+      else:
+        import traceback
+        traceback.print_exc()
+
+  _is_main_loop_running = False
 
   global _quit_handlers
   for cb in _quit_handlers:
